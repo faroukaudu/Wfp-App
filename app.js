dc9d3ed3105c7668325367db05b8c952393bbf2c
unknown <peternnadi1999@gmail.com>
2023-06-13 22:28:33 -1100

    fix merge conflict

--- app.js
+++ app.js
@@ -9,11 +9,6 @@ const superadmin = require(__dirname + "/admin/superadmin.js");
 
 
 
-<<<<<<< HEAD
-// app.use(express.static("public"));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.set("view engine", "ejs");
-=======
 app.get("/addUser", (req, res) => {
   res.render("bounds/createUsers");
 });
@@ -27,7 +22,6 @@ app.get("/report", (req, res) => {
 app.get("/submit", (req, res) => {
   res.render("bounds/formComplete");
 });
->>>>>>> 8bf55f1d0c99d3332c3f5937f2f328e71c271a80
 
 
 app.get("/admin", (req,res) =>{
